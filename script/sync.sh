8b2aa66c644e7bf1e2bc129b84da4603f52cae1f
typebrook <typebrook@gmail.com>
2019-04-18 10:48:30 +0800

    update

--- script/sync.sh
+++ script/sync.sh
@@ -1,2 +1,22 @@
+check_upstream() {
+
+    head='dev'
+    if [ $# -eq 2 ]
+    then
+        head=$2
+    fi
+
+    cd ~/$1
+    git fetch origin && \
+    git rev-list $head | grep $(git rev-parse origin/master) > /dev/null
+
+    if [ $? -ne 0 ]
+    then
+        echo "New commit at" $1
+    fi
+}
+
 cd ~/git/settings && git pull --quiet &
 cd ~/vimwiki && git pull --quiet &
+check_upstream git/tig &
+check_upstream .vim_runtime &
