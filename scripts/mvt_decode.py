8daf16baa75f048574216a21234f90363b56cdf5
typebrook <typebrook@gmail.com>
2019-08-15 23:51:40 +0800

    update

---
+++ scripts/mvt_decode.py
@@ -0,0 +1,14 @@
+import mapbox_vector_tile
+import sys
+
+#Python3
+
+mvt = sys.argv[1]
+
+print(mvt)
+
+with open(mvt, 'rb') as f:
+    data = f.read()
+    decoded_data = mapbox_vector_tile.decode(data)
+    with open(mvt + '_decode.txt', 'w') as f:
+        print(decoded_data)
