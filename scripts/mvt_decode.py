97c948ebd3e013cc2aabe54bf83a78bc89f860bb
typebrook <typebrook@gmail.com>
2019-08-20 20:09:38 +0800

    update

--- scripts/mvt_decode.py
+++ scripts/mvt_decode.py
@@ -8,5 +8,4 @@ mvt = sys.argv[1]
 with open(mvt, 'rb') as f:
     data = f.read()
     decoded_data = mapbox_vector_tile.decode(data)
-    with open(mvt + '_decode.txt', 'w') as f:
-        print(decoded_data)
+    print(decoded_data)
