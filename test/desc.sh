d627b3a4093a37740604daedcb45119f196b5fcd
typebrook <typebrook@gmail.com>
2019-09-18 13:43:06 +0800

    update

--- test/desc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for name in $(cat qlist)
-do
-    echo $name
-    wd set-description $name en "A cyclone given name adopted by by ESCAP/WMO Typhoon Committee"
-done
--- test/sh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for name in $(cat qlist)
-do
-    wd label $name | xargs -I{} grep {} name-list
-    echo $name
-done
