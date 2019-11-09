6c135c1235c3ace511baee440ac6289a9495ff2d
typebrook <typebrook@gmail.com>
2019-11-10 00:35:53 +0800

    update

--- scripts/load-settings.sh
+++ scripts/load-settings.sh
@@ -5,5 +5,11 @@ fi
 # load custom aliases
 source $SETTING_DIR/alias.sh
 
+# Add custom scripts into PATH
 PATH=$PATH:$SETTING_DIR/scripts
+
+# sync with important git repos
 $SETTING_DIR/scripts/sync.sh
+
+# load custom functions
+source $SETTING_DIR/utils/osm
