950c74ac1512f2b13396c622353dbfcaccb0bfe6
typebrook <typebrook@gmail.com>
2019-12-17 10:52:11 +0800

    update

--- scripts/load-settings.sh
+++ scripts/load-settings.sh
@@ -7,10 +7,15 @@ source $SETTING_DIR/alias
 
 # Add custom scripts into PATH
 PATH=$PATH:$SETTING_DIR/scripts
+find $SETTING_DIR/scripts -type d | sed 1d |\
+while read dir; do
+    PATH=$PATH:$dir
+done
 
 # sync with important git repos
 $SETTING_DIR/scripts/sync.sh
 
 # load custom functions
 OSM_UTIL_DIR=$SETTING_DIR/scripts/osm
-source $OSM_UTIL_DIR/osm && PATH=$PATH:$OSM_UTIL_DIR
+source $OSM_UTIL_DIR/osm
+
