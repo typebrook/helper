aa9ebaf1ab4b234a07de64f3965bdf74caaea6e6
typebrook <typebrook@gmail.com>
2020-02-05 19:19:17 +0800

    update

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd $SETTING_DIR && git pull --quiet & || echo in $SETTING_DIR
-cd ~/vimwiki && git pull --quiet & || echo in ~/vimwiki
-check_upstream ~/git/tig & || echo in ~/git/tig
-check_upstream ~/.vim_runtime & || echo in ~/.vim_runtime
+cd $SETTING_DIR && git pull --quiet || echo in $SETTING_DIR > /dev/tty &
+cd ~/vimwiki && git pull --quiet || echo in ~/vimwiki > /dev/tty &
+check_upstream ~/git/tig || echo in ~/git/tig > /dev/tty &
+check_upstream ~/.vim_runtime || echo in ~/.vim_runtime > /dev/tty &
