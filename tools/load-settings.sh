00b136155183ae522ca458e540a8cf29bf525e74
typebrook <typebrook@gmail.com>
2020-02-27 16:32:37 +0800

    update

--- tools/load-settings.sh
+++ tools/load-settings.sh
@@ -6,17 +6,17 @@ fi
 source $SETTING_DIR/alias
 
 # Add custom scripts into PATH
-PATH=$PATH:$SETTING_DIR/scripts
-find $SETTING_DIR/scripts -type d | sed 1d |\
+PATH=$PATH:$SETTING_DIR/tools
+find $SETTING_DIR/tools -type d | sed 1d |\
 while read dir; do
     PATH=$PATH:$dir
 done
 
 # sync with important git repos
-$SETTING_DIR/scripts/sync.sh
+$SETTING_DIR/tools/sync.sh
 
 # load custom functions
-OSM_UTIL_DIR=$SETTING_DIR/scripts/osm
+OSM_UTIL_DIR=$SETTING_DIR/tools/osm
 source $OSM_UTIL_DIR/osm
 
 # go
