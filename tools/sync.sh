e7598928bac80354cd2dcaca25f7849cb1e6b21c
typebrook <typebrook@gmail.com>
2020-03-05 21:06:33 +0800

    update

--- tools/sync.sh
+++ tools/sync.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# my repo
 cd $SETTING_DIR && git pull --quiet || echo in `pwd` > /dev/tty &
-cd ~/vimwiki && git pull --quiet || echo in `pwd` > /dev/tty &
+if [ -d ~/vimwiki ]; then
+  cd ~/vimwiki && git pull --quiet || echo in `pwd` > /dev/tty &
+fi
+
+# others repo
 check_upstream ~/git/tig || echo in `pwd` > /dev/tty &
 check_upstream ~/.vim_runtime || echo in `pwd` > /dev/tty &
