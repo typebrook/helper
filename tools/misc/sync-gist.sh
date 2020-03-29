5bef750938b17adaf8a89e3e6c110a7480848832
typebrook <typebrook@gmail.com>
2020-03-29 23:21:23 +0800

    update

---
+++ tools/misc/sync-gist.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+set -o pipefail
+set -e
+
+repo=~/gist/b0d2e7e67aa50298fdf8111ae7466b56
+
+while read -r commit; do
+
+  cd $repo
+  git checkout $commit
+  message="$(git show $commit | sed -n '1,4 d; /^diff/ q; s/^    //p')"
+
+  cd ~/git/Bash-Snippets
+  cp $repo/gist gist/gist
+  git add gist/gist && git commit -m "$message"
+done
+
