ca9acdbd6757ab87530df0cddfca982fc57e7fcb
typebrook <typebrook@gmail.com>
2020-05-04 09:55:34 +0800

    update

--- tools/swap-protocol.bash
+++ tools/swap-protocol.bash
@@ -3,15 +3,16 @@
 target=''
 extra=''
 
+# For each remote
 git remote -v \
 | while read remote url etc; do
-  [[ $etc =~ push ]] && extra='--push'
-  if [[ -z $target || $target == https ]] && [[ $url =~ git@.*github.com ]]; then
+  [[ $etc =~ push ]] && extra='--push' || extra=''
+  if [[ $url =~ git@.*github.com ]]; then
     target=${target:-https}
-    sed -E 's#git@(.+):(.+)#https://\1/\2#' <<<$url | xargs git remote set-url $extra $remote
-  elif [[ -z $target || $target == git ]] && [[ $url =~ https://.*github.com ]]; then
+    [[ $target == https ]] && sed -E 's#^git@(.+):(.+)$#https://\1/\2#' <<<$url | xargs git remote set-url $extra $remote
+  elif [[ $url =~ https://.*github.com ]]; then
     target=${target:-git}
-    sed -E 's#https://([^/]+)/(.+)#git@\1:\2#' <<<$url | xargs git remote set-url $extra $remote
+    [[ $target == git ]] && sed -E 's#^https://([^/]+)/(.+)$#git@\1:\2#' <<<$url | xargs git remote set-url $extra $remote
   fi
 done
 
