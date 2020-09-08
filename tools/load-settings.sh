6970e92982e844c17ea5e10e05b53ed9e70ac22f
Hsieh Chin Fan <typebrook@gmail.com>
2020-09-08 10:23:29 +0800

    update

--- tools/load-settings.sh
+++ tools/load-settings.sh
@@ -14,16 +14,17 @@ export EDITOR=vim
 # load custom aliases
 source $SETTING_DIR/alias
 
-# Add custom scripts into PATH
-mkdir -p $HOME/bin
-PATH=$PATH:$HOME/bin
-
-find $SETTING_DIR/tools -type f -executable | \
-xargs realpath | \
-xargs -I{} ln -sf {} $HOME/bin
-
 # sync with important git repos
 $SETTING_DIR/tools/sync.sh
+
+# Add custom scripts into PATH
+BIN_DIR=$HOME/bin
+PATH=$PATH:$BIN_DIR
+mkdir -p $BIN_DIR
+find $BIN_DIR -xtype l | xargs rm
+
+find $SETTING_DIR/tools -type f -executable | \
+xargs realpath | xargs -I{} ln -sf {} $BIN_DIR
 
 # load custom functions
 OSM_UTIL_DIR=$SETTING_DIR/tools/osm
@@ -31,4 +32,3 @@ source $OSM_UTIL_DIR/osm
 
 # go
 PATH=$PATH:$HOME/go/bin
-
