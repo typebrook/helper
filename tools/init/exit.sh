9768b65eae1689ed1f6abc216e8f240d6ee9f56c
Hsieh Chin Fan <typebrook@gmail.com>
2021-04-05 22:12:23 +0800

    update

--- tools/init/exit.sh
+++ tools/init/exit.sh
@@ -1,3 +1,5 @@
 #! /bin/env bash
 
-rsync -a --delete ~/.thunderbird/ pham@topo.tw:~/.thunderbird/ &
+if [[ `cat /etc/hostname` != 'vultr' ]]; then
+  rsync -a --delete ~/.thunderbird/ pham@topo.tw:~/.thunderbird/ &
+fi
