feff9da066006c502c92da88f777e9585b1c0ea3
Hsieh Chin Fan <typebrook@gmail.com>
2021-04-06 08:45:51 +0800

    update

--- tools/init/sync.sh
+++ tools/init/sync.sh
@@ -15,4 +15,6 @@ check_upstream ~/git/tig || echo in `pwd` >/dev/tty &
 check_upstream ~/.vim_runtime || echo in `pwd` >/dev/tty &
 
 # thunderbird
-rsync -a pham@topo.tw:~/.thunderbird/ ~/.thunderbird/ &
+if [[ `cat /etc/hostname` != 'vultr' ]]; then
+  rsync -a pham@topo.tw:~/.thunderbird/ ~/.thunderbird &
+fi
