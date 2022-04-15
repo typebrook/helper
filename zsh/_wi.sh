7bcdb84b1ebf1741c426947e811d32452ce6031f
Hsieh Chin Fan <typebrook@gmail.com>
2022-04-15 08:59:55 +0800

    Update

--- zsh/_wi.sh
+++ zsh/_wi.sh
@@ -1,7 +1,11 @@
 #compdef wi
 
 function _wi() {
-    compadd -S '' $(cd ~/vimwiki && find -not -path "./logseq/*" -name '*.md' -printf "%f\n" | sed -E '/^[0-9]{4}-[0-9]{2}-[0-9]{2}/d')
+    compadd -S '' $(
+      cd ~/vimwiki && \
+      find -not -path "./logseq/*" -name '*.md' -printf "%f\n" | \
+      sed -E '/^[0-9]{4}-[0-9]{2}-[0-9]{2}/d; s/.md$//'
+    )
 }
 
 _wi
--- zsh/_ww.sh
+++ zsh/_ww.sh
@@ -1,7 +1,11 @@
 #compdef ww
 
 function _ww() {
-    compadd -S '' $(cd ~/vimwiki && find -not -path "./logseq/*" -name '*.md' -printf "%f\n" | sed -E '/^[0-9]{4}-[0-9]{2}-[0-9]{2}/d')
+    compadd -S '' $(
+      cd ~/vimwiki && \
+      find -not -path "./logseq/*" -name '*.md' -printf "%f\n" | \
+      sed -E '/^[0-9]{4}-[0-9]{2}-[0-9]{2}/d; s/.md$//'
+    )
 }
 
 _ww
