dfe4efc3fab02f1d64f94d59d0aa36ae74cb7b4f
Hsieh Chin Fan <typebrook@gmail.com>
2022-07-12 10:57:41 +0800

    Update

--- zsh/_vp.sh
+++ zsh/_vp.sh
@@ -1,7 +1,7 @@
 #compdef vp
 
 function _vp() {
-  compadd -S '' $(find ~/blog/content -name '*.md' | grep -o '[^/]*$')
+  compadd -S '' $(ssh vps find /home/pham/blog/content -name '\*.md' | grep -o '[^/]*$')
 }
 
 _vp
