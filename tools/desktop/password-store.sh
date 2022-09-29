5853fe6dcb8b6eafe7b85d7ff7d6d7a9b230ad17
Hsieh Chin Fan <pham@topo.tw>
2022-09-29 17:51:56 +0800

    Update

--- tools/desktop/password-store.sh
+++ tools/desktop/password-store.sh
@@ -10,13 +10,19 @@ sed 's/.gpg$//' | \
 rofi -dmenu "${ROFI_ARGS[@]}" | {
   # Get arguments for command 'pass'
   read ARG1 ARG2
+  echo ARG1: $ARG1
+  echo ARG2: $ARG2
 
   if [[ -z $ARG1 ]]; then
     exit 1
-  elif [[ $ARG1 =~ gen ]]; then
-    # Generate a new password by ARG2
-    alacritty --hold -e pass --clip generate $ARG2
+  elif [[ $ARG1 == edit ]]; then
+    # Edit an existing password
+    alacritty --hold -e pass edit $ARG2 && \
+	rofi -e Password Edited: $ARG2
   else
+	# If pass fails, then it means password doesn't exists
+	set pipefail
+
     pass $ARG1 | {
       # If command fails, just fail directly
       read PASSWORD; [[ -z $PASSWORD ]] && exit 1
@@ -25,8 +31,20 @@ rofi -dmenu "${ROFI_ARGS[@]}" | {
       echo $PASSWORD | xsel -ib
 
       # Show success message, and display extra contents
-      rofi -e "Copied: $ARG1 $(echo; echo; cat | sed '1{/^$/d}')" \
-      "${ROFI_ARGS[@]}" 
-  }
+      rofi "${ROFI_ARGS[@]}" \
+        -e "Copied: $ARG1 $(echo; echo; cat | sed '1{/^$/d}')"
+    } || {
+	  # Make sure user want to create a new password
+      alacritty -e dialog --yesno \
+        "Password doesn't exist, Generate a new one?" 7 30 || exit 1
+
+	  # Generate a new password by ARG1
+	  alacritty -e pass generate $ARG1 --clip && \
+
+      # Show success message
+	  rofi "${ROFI_ARGS[@]}" -e "Password Created and Copied: $ARG1"
+	}
+
+	# TODO: if return code is 2, it means gpg password is not cached
   fi
 }
