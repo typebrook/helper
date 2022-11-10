66009d86cf2939d2a84d8643a00f4b82c3426437
Hsieh Chin Fan <pham@topo.tw>
2022-11-10 16:05:04 +0800

    Add script about mail

---
+++ X11/mail/check_mail.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+IMAP_SERVER=$1
+LOG_INFO=$2
+
+NUMBER=$(
+  curl -s -u $LOG_INFO $IMAP_SERVER -X 'STATUS INBOX (UNSEEN)' | \
+  sed -E 's/^.+\(UNSEEN ([0-9]+)\).+$/\1/' | \
+  tr -d '\r'
+)
+
+if [[ -n "$NUMBER" && $NUMBER -gt 0 ]]; then
+  DISPLAY=:0 \
+  DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/`id -u`/bus \
+  notify-send "New Mail: $NUMBER"
+else
+  date
+fi
