feee1a1bdd0afb5657a0e08198b6a35123c0bc38
Hsieh Chin Fan <pham@topo.tw>
2022-12-03 20:57:27 +0800

    Update script of mastodon

--- tools/sns/mastodon.sh
+++ tools/sns/mastodon.sh
@@ -1,12 +1,17 @@
 #! /bin/bash
 
+# Restore mail in variable
 MAIL="$(cat)"
 
 # Only execute the following script when mail receiver is mastodon@topo.tw
-grep -qE "^To: .*mastodon@topo.tw[>]?$" <<<"$MAIL" || exit 0
+grep -qE "^X-Original-To: .*mastodon@topo.tw[>]?$" <<<"$MAIL" || exit 0
+# A little hacky way to check if mail is sent from me
+sed -nE '/^Received: /p;/^$/q' <<<"$(MAIL)" | wc -l | xargs -i test {} -lt 2 || exit 0
 
+# Leave log
 date >>~/Downloads/mastodon.log
 echo $$ >>~/Downloads/mastodon.log
 awk -v RS= 'NR>1' <<<"$MAIL"  >>~/Downloads/mastodon.log
 
-#awk -v RS= 'NR>1' <<<"$MAIL" | toot post >/tmp/mastodon.log
+# Use toot to send message on g0v.social
+awk -v RS= 'NR>1' <<<"$MAIL" | toot post >>/tmp/mastodon.log
