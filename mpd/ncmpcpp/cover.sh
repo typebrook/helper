4b5abb8a21658623be26e1ad03712e65cae5a415
Hsieh Chin Fan <pham@topo.tw>
2023-02-28 23:11:08 +0800

    Add ncmpcpp layout
    ref: https://radumirea.com/blog/ncmpcpp-with-album-art

---
+++ mpd/ncmpcpp/cover.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source "`ueberzug library`"
+COVER="/tmp/album_cover.png"
+
+function add_cover {
+  ImageLayer::add [identifier]="img" [x]="2" [y]="1" [path]="$COVER"
+}
+
+ImageLayer 0< <(
+if [ ! -f "$COVER" ]; then
+  cp "$HOME/.ncmpcpp/default_cover.png" "$COVER"
+fi
+#rerender image when changed
+while inotifywait -q -q -e close_write "$COVER"; do
+  add_cover
+done
+)
