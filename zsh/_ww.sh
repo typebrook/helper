d073346d4252f1cdb57d002f888d49e9557db765
Hsieh Chin Fan <pham@topo.tw>
2023-03-06 11:19:51 +0800

    Update

--- zsh/_ww.sh
+++ zsh/_ww.sh
@@ -3,8 +3,8 @@
 function _ww() {
     compadd -S '' $(
       cd ~/log && \
-      find -not -path "./logseq/*" -name '*.md' -printf "%f\n" | \
-      sed -E '/^[0-9]{4}-[0-9]{2}-[0-9]{2}/d; s/.md$//'
+      find -name '*.md' | \
+      sed -E 's#./##; s/.md$//'
     )
 }
 
