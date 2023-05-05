fe8327560ca95571cf6f2ce4762377a112cd815d
Hsieh Chin Fan <pham@topo.tw>
2023-05-05 14:08:59 +0800

    Update

--- bin/wakeup.sh
+++ bin/wakeup.sh
@@ -1,19 +1,3 @@
 #! /bin/bash
 
-# Add custom scripts into PATH
-export SETTING_DIR=~/helper
-export BIN_DIR=$HOME/bin
-export PATH=$BIN_DIR:$PATH
-
-mkdir -p $BIN_DIR
-find $BIN_DIR -xtype l -exec rm {} + 2>/dev/null
-find $SETTING_DIR/bin -type f -executable -exec realpath {} + | \
-xargs -I{} ln -sf {} $BIN_DIR
-
-# sync with important git repos
-setsid ~/bin/sync.sh &>>/tmp/sync
-
-# Copy context file from vps
-rsync -au vps:~/.context ~/.context
-
-date >>/tmp/wakeup
+date --iso-8601=minutes >~/.wakeup
