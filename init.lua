4418f22aac80af84112ea7507480540e20c74385
Hsieh Chin Fan <pham@topo.tw>
2023-06-05 15:55:09 +0800

    Update

--- init.lua
+++ init.lua
@@ -286,7 +286,7 @@ vim.keymap.set('n', '<leader>q', require('telescope.builtin').buffers, { desc = 
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
+    -- winblend = 10,
     previewer = false,
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
