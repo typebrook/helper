19ab5f5cf66a18880eb5728786a76bb8bd9b9677
Hsieh Chin Fan <pham@topo.tw>
2023-08-04 23:16:32 +0800

    Update

---
+++ stylelint.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "plugins": [
+    "stylelint-order"
+  ],
+  "rules": {
+    "order/order": [
+      "custom-properties",
+      "declarations"
+    ],
+    "order/properties-order": [
+      "height",
+      "width",
+    ]
+  }
+}
