16fb5d9b284244b9ed16ae3a8d2c817d2acac0ab
Hsieh Chin Fan <pham@topo.tw>
2024-05-10 16:33:48 +0800

    Update

--- vim/kickstarter.lua
+++ vim/kickstarter.lua
@@ -365,7 +365,6 @@ vim.cmd("nnoremap <silent> <leader>z :Goyo<CR>")
 -- [[ Configure NERDTree ]]
 vim.g.NERDTreeWinPos = 'left'
 vim.g.NERDTreeShowHidden = 0
-vim.g.NERDTreeQuitOnOpen = 1
 vim.api.nvim_set_var('NERDTreeWinSize', 35)
 vim.cmd("map <C-n> :NERDTreeToggle<cr>")
 vim.cmd("map <leader>nb :NERDTreeFromBookmark<Space>")
