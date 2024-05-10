892810feb79d6f83c13208197964189abcc02f12
Hsieh Chin Fan <pham@topo.tw>
2024-05-10 17:10:18 +0800

    Update

--- vim/kickstarter.lua
+++ vim/kickstarter.lua
@@ -451,7 +451,6 @@ require('nvim-treesitter.configs').setup {
   auto_install = false,
 
   highlight = { enable = true },
-  indent = { enable = true },
   incremental_selection = {
     enable = true,
     keymaps = {
