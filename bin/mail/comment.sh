2ef7db0d4cc14267db389ac209f8d3753d14fe99
Hsieh Chin Fan <pham@topo.tw>
2024-12-05 12:18:42 +0800

    improve comment.sh

--- bin/mail/comment.sh
+++ bin/mail/comment.sh
@@ -1,41 +1,95 @@
 #! /bin/bash
 
+# Save incoming mail as comment
+# Usage:
+#   echo '|<PATH_TO_THIS_SCRIPT>' >>~/.forward
+
+mailto=${mailto:-$(whoami)@${HOSTNAME:?HOSTNAME is not specified}}
 output_dir=${output_dir:-/srv/http}
+
+# Check mail is for comment {{{
 
 # Restore mail into variable
 MAIL="$(tr -d '\r')"
-headers="$(<<<"$MAIL" sed '/^$/ q')"
-contents="$(<<<"$MAIL" sed -n '/^$/,$ p' | sed '1d')"
+header="$(<<<"$MAIL" sed '/^$/ q')"
+body="$(<<<"$MAIL" sed -n '/^$/,$ p' | sed '1d')"
+
+# determine mail is for comment by pattern
+pattern='^Subject: .*comment on (https?://)?([^/]+)?/([^ ]+)$'
+<<<"$header" grep -E "$pattern" >/dev/null || exit 0
+
+# }}}
+# Process header fields {{{
 
 # enable execute last command in pipe under current shell
 shopt -s lastpipe; set +m;
 
-# get route (target page of comment) and id
-<<<"$MAIL" sed -En '/^To: comment+/ {s/^To: comment\+([^+]+)\+?(.*)@.*$/\1 \2/p; q}; /^$/q' \
-| read route id
+# save each field of header into variables
+<<<"$header" grep '^[a-zA-Z]' \
+| while read field value; do
+  declare field=$(<<<$field tr [:lower:] [:upper:] | tr '-' '_' | tr -d ':')
+  declare $field="${value}"
+done
+DATE=${DATE:+$(date --rfc-3339 seconds --date "$DATE")}
 
-# sender want comment on some page, but find no route for this
-if [ $route = "" ]; then
-  echo 'rcpt "comment+<ROUTE>" not matched' >&2
+# }}}
+# Get path of output file {{{
+
+path=$(<<<"$header" sed -En "\\|${pattern}| {s//\\3/p; q}")
+
+# sender want comment on some page, but find no path for this
+if [ $path = "" ]; then
+  echo 'Cannot get target of comment from mail' >&2
   exit 1
 fi
 
-output=$output_dir/${route#/}.comment
-exec 1>>$output
+# get output path
+[[ "$path" =~ '/$' ]] && path+=index
+path=${path#/}
+path=${path/.html}
+output=$output_dir/${path}.comment.html
+
+# }}}
+# Get comment from mail body {{{
 
 # check mail includes multiple part
-<<<"$headers" grep '^Content-Type:.*mixed' >/dev/null
-if [ $? -eq 0 ]; then
-  boundary="$(<<<"$headers" sed -En 's/^Content-Type:.*boundary="(.*)".*$/\1/p')"
+if [[ "$CONTENT_TYPE" =~ mixed ]]; then
+  boundary="$(<<<"$CONTENT_TYPE" sed -En 's/^.*boundary="(.*)".*$/\1/p')"
   if [ $boundary = "" ]; then
     echo 'cannot get boundary from mail header' >&2
     exit 1
   fi
 
   # print content of first mail part
-  pattern="\\@^--${boundary}\$@"
-  <<<"$contents" sed -n "${pattern},${pattern} p" | sed -n "1,4d; ${pattern} q; p"
-else
-  # print content
-  <<<"$contents" sed '/^$/,$ p'
+  boundaryPat="\\|^--${boundary}\$|"
+  <<<"$body" sed -n "${boundaryPat},${boundaryPat} p" | sed -n "1,4d; ${boundaryPat} q; p" \
+  | read body
 fi
+
+# }}}
+# Write comment to output file {{{
+
+# add basic html layout for output file if necessary
+if [ ! -f $output ] || ! xmllint --html --nofixup-base-uris $output &>/dev/null; then
+  echo -e '<ul>\n</ul>' >$output
+fi
+# get line of insert position by header field "In-Reply-To"
+if [ -n "${IN_REPLY_TO}" ]; then
+  line=$(grep -n "^<!-- ${IN_REPLY_TO} -->$" $output | cut -d':' -f1)
+fi
+
+# insert comment into output file
+<<-COMMENT sed -i "${line:-1}r /dev/stdin" $output
+	<li>
+	<time datetime="${DATE}">${DATE}</time>
+	<a href="mailto:${mailto}?subject=comment on ${path}&in-reply-to=${MESSAGE_ID}">[reply]</a>
+
+	$(<<<"${body}" markdown)
+
+	<ul>
+	<!-- ${MESSAGE_ID} -->
+	</ul>
+	</li>
+COMMENT
+
+# }}}
