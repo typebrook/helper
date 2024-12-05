a539daebadf5f4a5058027f5be9d7cd4cbdee878
Hsieh Chin Fan <pham@topo.tw>
2024-12-05 20:41:23 +0800

    Update comment

--- bin/mail/comment.sh
+++ bin/mail/comment.sh
@@ -2,39 +2,37 @@
 
 # Save incoming mail as comment
 # Usage:
-#   echo '|<PATH_TO_THIS_SCRIPT>' >>~/.forward
+#   echo '|<PATH_TO_THIS_SCRIPT> --output_dir=<PATH_OF_HTML_FILES>' >>~/.forward
 
-set -x
-exec 2>>/dev/pts/1
-env >&2
-
-while [[ "$1" =~ ^-- && ! "$1" == "--" ]]; do
-  case $1 in
-    --mailto ) shift; mailto=$1 ;;
-    --output_dir ) shift; output_dir=$1 ;;
-  esac
-  shift
-done
-
-mailto=${mailto:-`whoami`@${HOSTNAME:?HOSTNAME is not specified}}
-[ -z "$mailto" ] && echo --mailto is not specified}
-[ -z "$output_dir" ] && echo --output_dir is not specified}
-markdown_bin=${markdown:-markdown}
-[ -x $(which $markdown_bin) ] || markdown_bin=cat
-
-# Check mail is for comment {{{
+# 1. Check mail is for comment {{{
 
 # Restore mail into variable
-MAIL="$(tee /tmp/mail | tr -d '\r')"
+MAIL="$(tr -d '\r')"
 header="$(<<<"$MAIL" sed '/^$/ q')"
 body="$(<<<"$MAIL" sed -n '/^$/,$ p' | sed '1d')"
 
 # determine mail is for comment by pattern
-pattern='^Subject: .*comment on (https?://)?([^/]+/)?([^ ]+)$'
+pattern='^Subject: .*[cC]omment on page: (https?://)?([^/]+/)?([^ ]+)$'
 <<<"$header" grep -E "$pattern" >/dev/null || exit 0
 
 # }}}
-# Process header fields {{{
+# 2. Get necessary variables from arguments {{{
+
+while [[ "$1" =~ ^-- && ! "$1" == "--" ]]; do
+  case $1 in
+    --output_dir ) shift; output_dir=$1 ;;
+    --markdown_bin ) shift; markdown_bin=$1 ;;
+    *) shift ;;
+  esac
+  shift
+done
+
+output_dir=${output_dir:?}
+markdown_bin=${markdown_bin:-markdown}
+[ -x $(which $markdown_bin) ] || markdown_bin=cat
+
+# }}}
+# 3. Read header fields {{{
 
 # enable execute last command in pipe under current shell
 shopt -s lastpipe; set +m;
@@ -48,7 +46,7 @@ done
 DATE=${DATE:+$(date --rfc-3339 seconds --date "$DATE")}
 
 # }}}
-# Get path of output file {{{
+# 4. Get path of output file {{{
 
 path=$(<<<"$header" sed -En "\\|${pattern}| {s//\\3/p; q}")
 
@@ -65,7 +63,7 @@ path=${path/.html}
 output=$output_dir/${path}.comment.html
 
 # }}}
-# Get comment from mail body {{{
+# 5. Get comment from mail body {{{
 
 # check mail includes multiple part
 if [[ "$CONTENT_TYPE" =~ mixed ]]; then
@@ -81,29 +79,60 @@ if [[ "$CONTENT_TYPE" =~ mixed ]]; then
 fi
 
 # }}}
-# Write comment to output file {{{
+# 6. Write comment to output file {{{
 
-# add basic html layout for output file if necessary
+# add basic html layout for output file if necessary {{{
 if [ ! -f $output ] || ! xmllint --html --nofixup-base-uris $output &>/dev/null; then
-  echo -e '<ul>\n</ul>' >$output
+  <<-LAYOUT cat >$output
+	<style>
+	  .comment-body {
+	    padding: 0.5rem;
+	    width: fit-content;
+	    border-radius: 8px;
+	    background: lightblue;
+	    p {
+	      margin: 0.5rem;
+	    }
+	  }
+	  .replies {
+	    display: none;
+	    cursor: pointer;
+	    &:has(li) {
+	      display: block;
+	    }
+	  }
+	</style>
+	<ul>
+	</ul>
+	LAYOUT
 fi
-# get line of insert position by header field "In-Reply-To"
+# }}}
+# get line of insert position by header field "In-Reply-To" {{{
 if [ -n "${IN_REPLY_TO}" ]; then
   line=$(grep -n "^<!-- ${IN_REPLY_TO} -->$" $output | cut -d':' -f1)
 fi
-
-# insert comment into output file
-<<-COMMENT sed -i "${line:-1}r /dev/stdin" $output
+# }}}
+# insert comment into output file {{{
+<<-COMMENT sed -i "${line:-/<ul>/}r /dev/stdin" $output
 	<li>
+
 	<time datetime="${DATE}">${DATE}</time>
-	<a href="mailto:${mailto}?subject=comment on ${path}&in-reply-to=${MESSAGE_ID}">[reply]</a>
+	<a href="mailto:${RECIPIENT}?subject=Comment on page: ${path}&in-reply-to=${MESSAGE_ID}">[reply]</a>
 
+	<div class='comment-body'>
 	$(<<<"${body}" ${markdown_bin})
+	</div>
 
+	<details class="replies" open="true">
+	<summary>replies</summary>
 	<ul>
 	<!-- ${MESSAGE_ID} -->
 	</ul>
+    </details>
+
 	</li>
 COMMENT
+
+# }}}
 
 # }}}
