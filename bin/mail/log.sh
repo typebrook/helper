5e34553daa746589233b7da1d5a468088569a5a7
Hsieh Chin Fan <pham@topo.tw>
2026-01-20 11:29:34 +0800

    Update

--- bin/mail/log.sh
+++ bin/mail/log.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
 
-# Get time of receiving mail
-epoch=$(date +%s)
-mail_date="$(date --rfc-email -d @${epoch})"
-
 # shell opt/trap {{{
 shopt -s nocasematch extglob
 
@@ -19,15 +15,61 @@ trap 'rm -rf ${tmp_mailbox}' EXIT
 MAIL="$(decodemail ${tmp_mailbox})"
 
 # Only execute the following script when mail receiver is log@topo.tw
-grep -qE "^Delivered-To: log@topo.tw$" <<<"$MAIL" && \
-grep -qE "^Subject: Message from Pham$" <<<"$MAIL" || \
-exit 0
+grep -qE -e '^Delivered-To: log@topo.tw$' -e '^ChatVersion' <<<"$MAIL" \
+|| exit 0
 
-<<<"$MAIL" sed -n '/^$/,$ p' | sed -n 2p >>~/LOG
+MESSAGE="$(<<<"$MAIL" sed -n '/^$/,$ p' | sed -n 2p)"
 # }}}
+# write message to log {{{
 
-#declare -i width_mailbox=$(wc -c <<<"${mailbox:-INBOX}")
-#spaces="$(printf %$(( 16 - ${width_mailbox} ))s)"
-#echo -e $(date '+%m/%d %H:%M' -d @${epoch}) "=> ${mailbox:-INBOX}" "$spaces" "${heading:-${SUBJECT}}" >&2
+if [[ ! "$MESSAGE" =~ ^[/:] ]]; then
+  [[ "$MESSAGE" =~ ^\. ]] && MESSAGE="${MESSAGE#.} #todo"
+  [[ "$MESSAGE" =~ ^\+ ]] && MESSAGE="${MESSAGE#+} #buy"
+
+  echo "$MESSAGE" >>~/LOG
+  exit 0
+fi
+# }}}
+# special char for commands {{{
+if [[ "$MESSAGE" =~ ^: ]]; then
+  line=$(cut -d' ' -f2 <<<"$MESSAGE")
+  case "$MESSAGE" in
+    # mark specific line as #done
+    :d* )
+      sed -i "$line s/ #todo/ #done:$(date --iso)/" ~/LOG
+      ;;
+    # rewrite specific line
+    :r* )
+      content="$(cut -d' ' -f3- <<<"$MESSAGE")"
+      sed -i "$line s/^.*$/$content/" ~/LOG
+      ;;
+    # append contents to specific line
+    :a* )
+      content="$(cut -d' ' -f3- <<<"$MESSAGE")"
+      sed -i "$line s/\$/ $content/" ~/LOG
+      ;;
+    # substitute word on specific line
+    :s* )
+      target="$(cut -d' ' -f3 <<<"$MESSAGE")"
+      dest="$(cut -d' ' -f4- <<<"$MESSAGE")"
+      sed -i "$line s/$target/$dest/" ~/LOG
+      ;;
+  esac
+  exit 0
+fi
+# }}}
+# special char for search {{{
+smtp pham@topo.tw <<EOF
+From: <log@topo.tw>
+Content-Type: text/plain; charset="utf-8"
+In-Reply-To: $(<<<"$MAIL" grep In-Reply-To: | head -1 | grep -o '<.*>')
+Message-ID: $(date --iso=seconds)
+Chat-Version: 1.0
+Chat-Disposition-Notification-To: pham@topo.tw
+Subject: Message from log@topo.tw
+
+$(<~/LOG nl | grep -i "${MESSAGE#/}")
+EOF
+# }}}
 
 # vim:fdm=marker fdl=0
