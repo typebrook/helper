15d876565b8f3859b58dc14e015d361fee5ff5e3
Hsieh Chin Fan <pham@topo.tw>
2026-02-09 09:44:54 +0800

    update log.sh

--- bin/mail/log.sh
+++ bin/mail/log.sh
@@ -1,17 +1,18 @@
 #! /bin/bash
 
-# DEBUG
+# DEBUG {{{
 export PS4='Line ${LINENO}: '
-exec &>log.log
 set -x
-
-# shell opt/trap {{{
+exec &>log.log
+# }}}
+# shell opt/var {{{
 shopt -s nocasematch extglob
 MAIL="$(cat)"
 
 # Only execute the following script when mail receiver is log@topo.tw
-grep -qE -e '^Delivered-To: log@topo.tw$' -e '^ChatVersion' <<<"$MAIL" \
-|| exit 0
+if [[ ! "$MAIL" =~ ^Delivered-To: log@topo.tw$ || ! "$MAIL" =~ '^ChatVersion' ]]; then
+  exit 0
+fi
 
 # MESSAGE: a single line message
 MESSAGE="$(<<<"$MAIL" sed -n '/^$/,$ p' | sed -n 2p)"
@@ -33,46 +34,47 @@ if [[ "$MESSAGE" =~ ^: ]]; then
     # HELP: ":d <LINE> 3" to tag as #done:<3-DAYS-BEFORE>
     # HELP: ":d <LINE> wed" to tag as #done:<LAST-WEDNESDAY >
     :d* )
-      time="$content"
-      case "$time" in
-        [[:digit:]]* ) date=$(date --iso --date="-${time}days") ;;
-        [[:alpha:]]* ) date=$(date --iso --date="last ${time}") ;;
-        * ) date=$(date --iso) ;;
-      esac
-      sed -Ei "$line_num s/ #(todo|done[^ ]*)/ #done:${date}/" ~/LOG
-      ;;&
+    time="$content"
+    case "$time" in
+      [[:digit:]]* ) date=$(date --iso --date="-${time}days") ;;
+      [[:alpha:]]* ) date=$(date --iso --date="last ${time}") ;;
+      * ) date=$(date --iso) ;;
+    esac
+    sed -Ei "$line_num s/ #(todo|done[^ ]*)/ #done:${date}/" ~/LOG
+    ;;&
     # HELP: ":r <LINE> <CONTENT>" to rewrite specific line
     :r* )
-      sed -i "$line_num s/^.*$/$content/" ~/LOG
-      ;;&
+    sed -i "$line_num s/^.*$/$content/" ~/LOG
+    ;;&
     # HELP: ":a <LINE> <CONTENT>" to append contents
     :a* )
-      sed -i "$line_num s/\$/ $content/" ~/LOG
-      ;;&
+    sed -i "$line_num s/\$/ $content/" ~/LOG
+    ;;&
     # HELP: ":s <TARGET> <DEST>" to substitute a word
     :s* )
-      target="$(cut -d' ' -f3 <<<"$MESSAGE")"
-      dest="$(cut -d' ' -f4- <<<"$MESSAGE")"
-      sed -i "$line_num s/$target/$dest/" ~/LOG
-      ;;&
+    target="$(cut -d' ' -f3 <<<"$MESSAGE")"
+    dest="$(cut -d' ' -f4- <<<"$MESSAGE")"
+    sed -i "$line_num s/$target/$dest/" ~/LOG
+    ;;&
     # HELP: ":o <LINE> <MESSAGE>" to add a list item
     :o* )
-      content="- $content"
-      sed -Ei "${line_num}"'a\'"$content" ~/LOG
-      ;;&
-    * )
-      exit 0
-      ;;
-  esac
+    content="- $content"
+    sed -Ei "${line_num}"'a\'"$content" ~/LOG
+    ;;&
+  * )
+    exit 0
+    ;;
+esac
 fi
 # }}}
 # reply something from query {{{
 
 # special char for metadata
-  # HELP: "#t" to list all tags
 if [ "$MESSAGE" = '#t' ]; then
+  # HELP: "#t" to list all tags
   REPLY="$(<LOG grep -Eo '#[^#: ]+' | sort | uniq -c | sort -n)"
 elif [ "$MESSAGE" = '#c' ]; then
+  # HELP: "#c" to list all commands
   REPLY="$(<$0 sed -En '/^ *# HELP: / {s/[^:]+:(.*)/\1\n/; p}')"
 elif [[ "$MESSAGE" =~ ^/ ]]; then
   # HELP: "/<WORD>" to search by string
@@ -103,13 +105,13 @@ fi
 if [ -n "$DATE" ]; then
   line_num=$(<~/LOG grep -n "^## $DATE" | cut -d: -f1)
   sed -i "${line_num}i $(cut -d' ' -f2-)" ~/LOG
-else [ -n "$MESSAGE" ]
-# HELP: "." to add tag #todo
-[[ "$MESSAGE" =~ ^\. ]] && MESSAGE="${MESSAGE#.} #todo"
-# HELP: "+" to add tag #buy
-[[ "$MESSAGE" =~ ^\+ ]] && MESSAGE="${MESSAGE#+} #buy"
+elif [ -n "$MESSAGE" ]
+  # HELP: "." to add tag #todo
+  [[ "$MESSAGE" =~ ^\. ]] && MESSAGE="${MESSAGE#.} #todo"
+  # HELP: "+" to add tag #buy
+  [[ "$MESSAGE" =~ ^\+ ]] && MESSAGE="${MESSAGE#+} #buy"
 
-echo "$MESSAGE" >>~/LOG
+  echo "$MESSAGE" >>~/LOG
 fi
 # }}}
 
