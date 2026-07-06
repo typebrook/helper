83f1266008373f99d9c22b2c326cc891e7394c0b
Hsieh Chin Fan <pham@topo.tw>
2026-07-06 07:56:31 -0400

    Update

--- X11/notify_status.sh
+++ X11/notify_status.sh
@@ -3,4 +3,4 @@
 capacity=$(cat ${POWER_CAPACITY:-/sys/class/power_supply/CMB0/capacity})
 capacity=${capacity}${capacity:+%}
 
-notify-send -t 2000 "$(date +%H:%M)		${capacity}"
+notify-send -t 1200 "$(date +%H:%M)		${capacity}"
